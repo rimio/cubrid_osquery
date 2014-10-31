8613ec921d64bf496de289f0a9df5533980105a6
Rimio <vasi.vilvoiu@gmail.com>
2014-10-31 15:37:30 +0200

    Added etc_hosts table and osquery_snapshot() procedure.

--- cubrid_osquery.java
+++ cubrid_osquery.java
@@ -18,6 +18,12 @@ public class cubrid_osquery
 		delStmt.close();	
 	}
 
+	public static void osquery_snapshot() throws SQLException
+	{
+		osquery_load_processes();
+		osquery_load_etc_hosts();
+	}
+
 	public static void osquery_load_processes() throws SQLException
 	{
 		try
@@ -60,7 +66,73 @@ public class cubrid_osquery
 			stmt.executeBatch();
 			stmt.close();
 			c.commit();
-			c.close();
+		}
+		catch (Exception e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
+
+	public static void osquery_load_etc_hosts() throws SQLException
+	{
+		try
+		{
+			Connection c = getConnection();
+			truncateTable(c, "_osquery_etc_hosts");
+
+			// Prepare insert statement
+			PreparedStatement stmt = c.prepareStatement("INSERT INTO _osquery_etc_hosts (address, hostnames) VALUES (?, ?)");
+
+			// Open file and parse lines
+			BufferedReader input = new BufferedReader(new FileReader(new File("/etc/hosts")));
+			String line;
+
+			while ((line = input.readLine()) != null)
+			{
+				if (line.length() <= 0)
+				{
+					// Empty line
+					continue;
+				}
+
+				// Normalize separators
+				line = line.replaceAll("^\\s+", "");
+				line = line.replaceAll("\\s+", " ");
+
+				if (line.startsWith("#"))
+				{
+					// Comment line
+					continue;
+				}
+
+				// Split
+				String[] split = line.split(" ");
+
+				if (split.length < 2)
+				{
+					// Malformed line
+					continue;
+				}
+
+				// Set address
+				stmt.setString(1, split[0]);
+
+				// Join hostnames
+				String hostnames = split[1];
+				for (int i = 2; i < split.length; i ++)
+				{
+					hostnames = hostnames + " " + split[i];
+				}
+
+				// Set hostnames
+				stmt.setString(2, hostnames);
+				stmt.addBatch();
+			}
+
+			stmt.executeBatch();
+			stmt.close();
+			c.commit();
+
 		}
 		catch (Exception e)
 		{
--- schema.sql
+++ schema.sql
@@ -1,15 +1,25 @@
 -- delete tables and views
 DROP VIEW IF EXISTS osquery_processes;
+DROP VIEW IF EXISTS osquery_etc_hosts;
 DROP TABLE IF EXISTS _osquery_processes;
+DROP TABLE IF EXISTS _osquery_etc_hosts;
 
 -- delete stored procedures
 DROP PROCEDURE osquery_load_processes;
+DROP PROCEDURE osquery_load_etc_hosts;
+
+DROP PROCEDURE osquery_snapshot;
 
 -- create stored procedures
 CREATE PROCEDURE osquery_load_processes() AS LANGUAGE JAVA NAME 'cubrid_osquery.osquery_load_processes()';
+CREATE PROCEDURE osquery_load_etc_hosts() AS LANGUAGE JAVA NAME 'cubrid_osquery.osquery_load_etc_hosts()';
+
+CREATE PROCEDURE osquery_snapshot() AS LANGUAGE JAVA NAME 'cubrid_osquery.osquery_snapshot()';
 
 -- create tables
 CREATE TABLE _osquery_processes (pid INT, name VARCHAR(255));
+CREATE TABLE _osquery_etc_hosts (address VARCHAR(50), hostnames VARCHAR(4096));
 
 -- create views
 CREATE VIEW osquery_processes AS SELECT * FROM _osquery_processes WHERE osquery_load_processes() IS NULL;
+CREATE VIEW osquery_etc_hosts AS SELECT * FROM _osquery_etc_hosts WHERE osquery_load_etc_hosts() IS NULL;
